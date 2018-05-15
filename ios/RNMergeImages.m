3c64513946e21291f3eaca88420add0ab599a21a
Florian Renner <f.renner@zetes.at>
2018-05-15 14:56:25 +0200

    IOS memory optimization (in case of memory issues, need to set scale to 1?)

--- ios/RNMergeImages.m
+++ ios/RNMergeImages.m
@@ -7,7 +7,8 @@
 - (NSString *)saveImage:(UIImage *)image {
     NSString *fileName = [[NSProcessInfo processInfo] globallyUniqueString];
     NSString *fullPath = [NSString stringWithFormat:@"%@%@.jpg", NSTemporaryDirectory(), fileName];
-    NSData *imageData = UIImagePNGRepresentation(image);
+    // Compress image to specified size
+    NSData *imageData = [self compressTo:10 image:image];
     [imageData writeToFile:fullPath atomically:YES];
     return fullPath;
 }
@@ -57,7 +58,6 @@ RCT_EXPORT_METHOD(merge:(NSArray *)imagePaths
         case 1:
             newImage = [self collage: imagePaths];
             break;
-            
         default:
             newImage = [self merge: imagePaths];
             break;
@@ -104,98 +104,121 @@ RCT_EXPORT_METHOD(merge:(NSArray *)imagePaths
 
 // Create collage from images (Setting: mergeType.COLLAGE)
 - (UIImage *)collage:(NSArray *)imagePaths {
-    @try {
-        NSMutableArray *imagesData = [[NSMutableArray alloc]init];
-        NSMutableArray *maxRowHeights = [[NSMutableArray alloc]init];
-        NSMutableArray *maxColumnWidths = [[NSMutableArray alloc]init];
-        NSInteger columnWitdhSum = 0, rowHeightSum = 0, tempMaxRowHeight = 0;
+    @autoreleasepool {
+        @try {
+            NSMutableArray *imagesMetadata = [[NSMutableArray alloc]init];
+            NSMutableArray *maxRowHeights = [[NSMutableArray alloc]init];
+            NSMutableArray *maxColumnWidths = [[NSMutableArray alloc]init];
+            NSInteger columnWitdhSum = 0, rowHeightSum = 0, tempMaxRowHeight = 0;
 
-        // Get image metaData, ignore images without metadata
-        for (id tempObject in imagePaths) {
-            NSURL *URL = [RCTConvert NSURL:tempObject];
-            NSData *imgData = [[NSData alloc] initWithContentsOfURL:URL];
-            if (imgData != nil) {
-                UIImage *image = [[UIImage alloc] initWithData:imgData];
-                [imagesData addObject:image];
-            }
-        }
-        
-        // Calc grid dimensions
-        NSInteger staticColumns = 2; // TODO: Works with all kinds of columns, could be added as feature to set column parameter
-        NSInteger imagesDataSize = [imagesData count];
-        NSInteger columns = imagesDataSize >= staticColumns ? staticColumns : imagesDataSize;
-        NSInteger rows = (NSInteger)ceilf((float) imagesDataSize / (float)columns);
-        
-        // Calculate contentSize dimensions
-        for (int i = 0; i < (int)[imagesData count]; i++) {
-            UIImage *imageData = [imagesData objectAtIndex: i];
-            @try {
-                if ([[maxColumnWidths objectAtIndex: (i % columns)] integerValue] < imageData.size.width) {
-                    [maxColumnWidths replaceObjectAtIndex:(i % columns) withObject: @(imageData.size.width)];
+            // Get image metaData, ignore images without metadata
+            for (id tempObject in imagePaths) {
+                NSURL *URL = [RCTConvert NSURL:tempObject];
+                NSData *imgData = [[NSData alloc] initWithContentsOfURL:URL];
+                if (imgData != nil) {
+                    UIImage *image = [[UIImage alloc] initWithData:imgData];
+                    [imagesMetadata addObject:@{ @"width": @(image.size.width), @"height": @(image.size.height), @"url": URL}];
                 }
             }
-            @catch (NSException *exception) {
-                // If no value is set
-                [maxColumnWidths insertObject: @(imageData.size.width) atIndex: i % columns];
+            
+            // Calc grid dimensions
+            NSInteger staticColumns = 2; // TODO: Works with all kinds of columns, could be added as feature to set column parameter
+            NSInteger imagesDataSize = [imagesMetadata count];
+            NSInteger columns = imagesDataSize >= staticColumns ? staticColumns : imagesDataSize;
+            NSInteger rows = (NSInteger)ceilf((float) imagesDataSize / (float)columns);
+            
+            // Calculate contentSize dimensions
+            for (int i = 0; i < (int)[imagesMetadata count]; i++) {
+                NSDictionary *imageMetadata = [imagesMetadata objectAtIndex: i];
+                @try {
+                    if ([[maxColumnWidths objectAtIndex: (i % columns)] integerValue] < [imageMetadata[@"width"] floatValue]) {
+                        [maxColumnWidths replaceObjectAtIndex:(i % columns) withObject: imageMetadata[@"width"]];
+                    }
+                }
+                @catch (NSException *exception) {
+                    // If no value is set
+                    [maxColumnWidths insertObject: imageMetadata[@"width"] atIndex: i % columns];
+                }
+            
+                if (tempMaxRowHeight < [imageMetadata[@"height"] floatValue]) {
+                    tempMaxRowHeight = [imageMetadata[@"height"] floatValue];
+                }
+                if ((i + 1) % columns == 0 || i + 1 == [imagesMetadata count]) {
+                    rowHeightSum += tempMaxRowHeight;
+                    [maxRowHeights addObject: @(tempMaxRowHeight)];
+                    tempMaxRowHeight = 0;
+                }
             }
         
-            if (tempMaxRowHeight < imageData.size.height) {
-                tempMaxRowHeight = imageData.size.height;
-            }
-            if ((i + 1) % columns == 0 || i + 1 == [imagesData count]) {
-                rowHeightSum += tempMaxRowHeight;
-                [maxRowHeights addObject: @(tempMaxRowHeight)];
-                tempMaxRowHeight = 0;
-            }
-        }
-    
-        // Sum up maxColumnWidths
-        for (NSNumber *maxColumnWidth in maxColumnWidths) {
-            columnWitdhSum += [maxColumnWidth integerValue];
-        }
-        
-        // Create context with size
-        NSInteger spacing = 20;
-        CGFloat width = columnWitdhSum + ((columns + 1) * spacing);
-        CGFloat height = rowHeightSum + ((rows + 1) * spacing);
-        CGSize contextSize = CGSizeMake(width, height);
-        UIGraphicsBeginImageContext(contextSize);
-        // Set background color
-        [[UIColor blackColor] set];
-        UIRectFill(CGRectMake(0.0, 0.0, width, height));
-        
-        // Merge images
-        NSInteger left = spacing, top = spacing, centerPaddingXSum = 0;
-        
-        for (int i = 0; i < (int)[imagesData count]; i++) {
-            UIImage *imageData = [imagesData objectAtIndex: i];
-            // position correctly
-            if (i > 0 && columns > 0 && i % columns == 0) {
-                left = spacing;
-                centerPaddingXSum = 0;
-                top += spacing + [[maxRowHeights objectAtIndex:((i - 1) / columns)] integerValue];
+            // Sum up maxColumnWidths
+            for (NSNumber *maxColumnWidth in maxColumnWidths) {
+                columnWitdhSum += [maxColumnWidth integerValue];
             }
             
-            NSInteger centerPaddingX = ([[maxColumnWidths objectAtIndex:(i % columns)] integerValue] - imageData.size.width) / 2;
-            NSInteger centerPaddingY = ([[maxRowHeights objectAtIndex:(i / columns)] integerValue] - imageData.size.height) / 2;
-            NSInteger canvasLeft = left + centerPaddingXSum + centerPaddingX;
-            NSInteger canvasTop = top + centerPaddingY;
-            NSInteger canvasWidth =  imageData.size.width;
-            NSInteger canvasHeight = imageData.size.height;
+            // Create context with size and black background
+            NSInteger spacing = 20;
+            CGFloat width = columnWitdhSum + ((columns + 1) * spacing);
+            CGFloat height = rowHeightSum + ((rows + 1) * spacing);
+            CGSize contextSize = CGSizeMake(width, height);
+            UIGraphicsBeginImageContextWithOptions(contextSize, YES, 0.0);
+            // Set background color
+            [[UIColor whiteColor] set];
+            UIRectFill(CGRectMake(0.0, 0.0, width, height));
+            // Merge images
+            NSInteger left = spacing, top = spacing, centerPaddingXSum = 0;
+            
+            for (int i = 0; i < (int)[imagesMetadata count]; i++) {
+                NSDictionary * imageMetadata = [imagesMetadata objectAtIndex: i];
+                NSData *imageData = [[NSData alloc] initWithContentsOfURL:imageMetadata[@"url"]];
+                UIImage *image = [[UIImage alloc] initWithData:imageData];
+                // position correctly
+                if (i > 0 && columns > 0 && i % columns == 0) {
+                    left = spacing;
+                    centerPaddingXSum = 0;
+                    top += spacing + [[maxRowHeights objectAtIndex:((i - 1) / columns)] integerValue];
+                }
+                
+                NSInteger centerPaddingX = ([[maxColumnWidths objectAtIndex:(i % columns)] integerValue] - [imageMetadata[@"width"] floatValue]) / 2;
+                NSInteger centerPaddingY = ([[maxRowHeights objectAtIndex:(i / columns)] integerValue] - [imageMetadata[@"height"] floatValue]) / 2;
+                NSInteger canvasLeft = left + centerPaddingXSum + centerPaddingX;
+                NSInteger canvasTop = top + centerPaddingY;
+                NSInteger canvasWidth =  [imageMetadata[@"width"] floatValue];
+                NSInteger canvasHeight = [imageMetadata[@"height"] floatValue];
 
-            [imageData drawInRect:CGRectMake(canvasLeft, canvasTop, canvasWidth, canvasHeight)];
-            centerPaddingXSum += 2 * centerPaddingX;
-            left += spacing + imageData.size.width;
+                [image drawInRect:CGRectMake(canvasLeft, canvasTop, canvasWidth, canvasHeight)];
+                centerPaddingXSum += 2 * centerPaddingX;
+                left += spacing + [imageMetadata[@"width"] floatValue];
+            }
+            // creating final image
+            UIImage *collageImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            
+            return collageImage;
         }
-        
-        // creating final image
-        UIImage *collageImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return collageImage;
+        @catch (NSException *exception) {
+            NSLog(@"%@", exception.reason);
+        }
+        return nil;
     }
-    @catch (NSException *exception) {
-        NSLog(@"%@", exception.reason);
+}
+
+- (NSData *)compressTo:(NSInteger)sizeInMb image:(UIImage *)soucreImage {
+    NSInteger sizeInBytes = sizeInMb * 1024 * 1024;
+    bool needCompress = true;
+    CGFloat compressingValue = 1.0;
+
+    while (needCompress && compressingValue > 0.0) {
+        NSData *imageData = UIImageJPEGRepresentation(soucreImage, compressingValue);
+        if (imageData) {
+            if ([imageData length] <= sizeInBytes) {
+                needCompress = false;
+                return imageData;
+            } else {
+                compressingValue -= 0.1;
+            }
+        }
     }
     return nil;
 }
+
 @end
